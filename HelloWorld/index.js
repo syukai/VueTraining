c5cbd738c7084c47ac32b7edac8680632b1b1445
d_hirayama <d_hirayama@rapid.ocn.ne.jp>
2017-07-25 11:55:55 +0900

    入れ子構造にしてclickに反応して色が変わるtd

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -93,3 +93,71 @@ var appcompute = new Vue({
         }
     }
 });
+
+
+var worklist=
+    {
+        props:['worktime'],
+        // template:'<tr><th>{{worktime.name}}</th><td is="workbox" v-for="n in 10" v-bind:{worktime:worktime,index:n}>&nbsp;</td></tr>'
+        // template:'<tr><th>{{worktime.name}}</th><td v-for="n in 10">&nbsp;</td></tr>'
+        // template:'<tr>\
+        //     <th>{{worktime.name}}</th>\
+        //     <td v-for="n in 10">&nbsp;</td>\
+        //     </tr>'
+        // template:'<tr>\
+        //     <th>{{worktime.name}}</th>\
+        //     <td is="workbox" v-for="n in 10" v-bind:worktime=worktime></td>\
+        //     </tr>'
+        template:'<tr>\
+            <th>{{worktime.name}}</th>\
+            <workbox v-for="n in 10" v-bind:worktime=worktime></workbox>\
+            </tr>'
+        // template:'<tr>\
+        //     <th>{{worktime.name}}</th>\
+        //     <td v-for="n in 10">\
+        //         <ol>\
+        //             <todo-item v-bind:todo="worktime"></todo-item>\
+        //         </ol>\
+        //     </td>\
+        //     </tr>'
+            
+    };
+// var workbox={
+Vue.component('workbox',{
+    props:['worktime'],
+    template:'<td v-on:click="toggleActive" v-bind:class="{active: isActive}">&nbsp;{{worktime.text}}</td>',
+    data:function(){return {isActive:false}
+    }
+,
+    methods:{
+        toggleActive:function(){
+            this.isActive=!this.isActive;
+        }
+    }
+});
+var myappWithComponent = new Vue({
+    el:'#myapp-with-component',
+    data:{
+        workers:[
+            {workerid:'1',name:'山田',st:3,ed:5,text:'yamada'},
+            {workerid:'2',name:'田中',st:1,ed:4,text:'tanaka'},
+            {workerid:'3',name:'鈴木',st:2,ed:8,text:'suzuki'}
+            ]
+    },
+    components:{
+        // 'workbox':workbox,
+        'worklist':worklist
+    }
+});
+// Vue.component('todo-item',{
+//     props:['todo'],
+//     template:'<li>{{todo.text}}</li>'
+// });
+// Vue.component('terms-of-service', {
+//   template: '\
+//     <div v-once>\
+//       <h1>Terms of Service</h1>\
+//       ... a lot of static content ...\
+//     </div>\
+//   '
+// })
